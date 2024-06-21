ac6aeb97589a1137a54265f448493b2696ba3ec3
Jie clark Terec <jieclarkm@gmail.com>
2024-06-22 00:21:02 +0800

    trying to fix url image

--- listings/serializers.py
+++ listings/serializers.py
@@ -1,7 +1,54 @@
+# from rest_framework import serializers
+# from .models import Item
+# from django.contrib.auth import get_user_model
+# from django.utils.translation import gettext_lazy as _
+
+# class ItemSerializer(serializers.ModelSerializer):
+#     user = serializers.PrimaryKeyRelatedField(read_only=True)
+#     reserved_by = serializers.PrimaryKeyRelatedField(
+#         queryset=get_user_model().objects.all(), 
+#         required=False,
+#         allow_null=True
+#     )
+
+#     user_first_name = serializers.SerializerMethodField()
+#     user_last_name = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = Item
+#         fields = ['id', 'user', 'user_first_name', 'user_last_name', 'name', 'description', 'category', 'condition', 'image', 'available', 'created_at', 'reserved', 'reserved_by']
+
+#     def get_user_first_name(self, obj):
+#         return obj.user.first_name if obj.user else None
+
+#     def get_user_last_name(self, obj):
+#         return obj.user.last_name if obj.user else None
+
+    
+
+#     # class Meta:
+#     #     model = Item
+#     #     fields = '__all__'
+
+#     # def validate_reserved_by(self, value):
+#     #     if value == self.context['request'].user:
+#     #         raise serializers.ValidationError(_("You cannot reserve your own item."))
+#     #     return value
+
+
+#     def update(self, instance, validated_data):
+#         if 'reserved_by' in validated_data:
+#             user = self.context['request'].user
+#             if validated_data['reserved_by'] == user:
+#                 raise serializers.ValidationError(_("You cannot reserve your own item."))
+#         return super().update(instance, validated_data)
+
+
 from rest_framework import serializers
-from .models import Item
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from .models import Item  
 
 class ItemSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
@@ -13,6 +60,7 @@ class ItemSerializer(serializers.ModelSerializer):
 
     user_first_name = serializers.SerializerMethodField()
     user_last_name = serializers.SerializerMethodField()
+    image = serializers.SerializerMethodField()  # Add this line
 
     class Meta:
         model = Item
@@ -24,7 +72,19 @@ class ItemSerializer(serializers.ModelSerializer):
     def get_user_last_name(self, obj):
         return obj.user.last_name if obj.user else None
 
-    
+    def get_image(self, obj):  # Add this method
+        request = self.context.get('request')
+        if obj.image and request:
+            return request.build_absolute_uri(obj.image.url)
+        return None
+
+    def update(self, instance, validated_data):
+        if 'reserved_by' in validated_data:
+            user = self.context['request'].user
+            if validated_data['reserved_by'] == user:
+                raise serializers.ValidationError(_("You cannot reserve your own item."))
+        return super().update(instance, validated_data)
+
 
     # class Meta:
     #     model = Item
@@ -34,14 +94,5 @@ class ItemSerializer(serializers.ModelSerializer):
     #     if value == self.context['request'].user:
     #         raise serializers.ValidationError(_("You cannot reserve your own item."))
     #     return value
-
-
-    def update(self, instance, validated_data):
-        if 'reserved_by' in validated_data:
-            user = self.context['request'].user
-            if validated_data['reserved_by'] == user:
-                raise serializers.ValidationError(_("You cannot reserve your own item."))
-        return super().update(instance, validated_data)
-
 
     
--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,47 @@
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+# from rest_framework import generics
+# from rest_framework.response import Response
+# from django_filters.rest_framework import DjangoFilterBackend
+# from rest_framework.views import APIView
+# from .models import Item
+# from .serializers import ItemSerializer
+# from rest_framework import status
+
+# class ItemListCreateView(generics.ListCreateAPIView):
+#     queryset = Item.objects.all()
+#     serializer_class = ItemSerializer
+#     permission_classes = [IsAuthenticated]
+#     filter_backends = [DjangoFilterBackend] 
+#     filterset_fields = ['category'] 
+#     search_fields = ['name', 'description']
+
+#     def perform_create(self, serializer):
+#         serializer.save(user=self.request.user)
+
+# class ItemRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Item.objects.all()
+#     serializer_class = ItemSerializer
+#     permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly]
+
+#     def perform_update(self, serializer):
+#         instance = serializer.save()  
+
+# class UserItemList(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def get(self, request):
+#         items = Item.objects.filter(user=request.user)
+#         serializer = ItemSerializer(items, many=True)
+#         return Response(serializer.data)
+
+#     def delete(self, request, pk):
+#         try:
+#             item = Item.objects.get(id=pk, user=request.user)
+#         except Item.DoesNotExist:
+#             return Response({"error": "Item not found or does not belong to the user"}, status=status.HTTP_404_NOT_FOUND)
+#         item.delete()
+#         return Response({"message": "Item deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics
 from rest_framework.response import Response
@@ -31,7 +75,7 @@ class UserItemList(APIView):
 
     def get(self, request):
         items = Item.objects.filter(user=request.user)
-        serializer = ItemSerializer(items, many=True)
+        serializer = ItemSerializer(items, many=True, context={'request': request})  # Add context here
         return Response(serializer.data)
 
     def delete(self, request, pk):
@@ -41,4 +85,3 @@ class UserItemList(APIView):
             return Response({"error": "Item not found or does not belong to the user"}, status=status.HTTP_404_NOT_FOUND)
         item.delete()
         return Response({"message": "Item deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
-
