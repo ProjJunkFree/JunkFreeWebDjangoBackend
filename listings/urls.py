6b782a3c9c00925e9957abe64c6b2ba4105a4d76
Jie clark Terec <jieclarkm@gmail.com>
2024-06-22 00:09:15 +0800

    modified urls.py on listing app

--- listings/urls.py
+++ listings/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('myitems/', views.UserItemList.as_view(), name='my_items'),
     path('myitems/<int:pk>/', views.UserItemList.as_view(), name='user_item_detail')
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
